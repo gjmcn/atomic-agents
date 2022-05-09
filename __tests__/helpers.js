530aed81e24e438071f586ca33bb99dc1acc4a8c
gmcn <gjmcn@users.noreply.github.com>
2022-05-09 12:01:38 +0100

    helpers

--- __tests__/helpers.js
+++ __tests__/helpers.js
@@ -23,7 +23,10 @@ import {
   partitionRect
 } from '../src/helpers';
 
+import { XSet } from '../src/x-set';
 import { random } from '../src/random';
+import { Simulation } from '../src/simulation';
+import { Actor } from '../src/actor';
 
 test('shuffle, 1', () => {
   random.seed(0.5);
@@ -128,5 +131,37 @@ test('randomElement, 1', () => {
   expect(randomElement(a)).toBe(730);
 });
 
+test('randomPointInCircle, 1', () => {
+  random.seed(0.5);
+  const p = randomPointInCircle(1, -2, 3);
+  const v = [ p.x.toFixed(3), p.y.toFixed(3) ];
+  expect(v).toStrictEqual(['0.153', '-0.844']);
+});
+
+test('getIndexLimits, 1', () => {
+  expect(getIndexLimits([2, 5, 1, 7]))
+    .toStrictEqual([2, 5, 1, 7]);
+});
+
+test('getIndexLimits, 2', () => {
+  expect(getIndexLimits({
+    yMinIndex: 1, yMaxIndex: 7, xMinIndex: 2, xMaxIndex: 5
+  })).toStrictEqual([2, 5, 1, 7]);
+});
+
+test('getOverlapping, 1', () => {
+  const sim = new Simulation({width: 100, height: 100, gridStep: 10});
+  const a = [
+    new Actor({x: 80, y: 10, radius:  3, state: { index: 0 }}).addTo(sim),
+    new Actor({x: 60, y: 70, radius: 10, state: { index: 1 }}).addTo(sim),
+    new Actor({x: 15, y: 35, radius: 20, state: { index: 2 }}).addTo(sim),
+    new Actor({x:  0, y:  0, radius: 12, state: { index: 3 }}).addTo(sim)
+  ];
+  const squaresRow1 = sim.squares.filter(sq => sq.yIndex === 1);
+  const indices = [...getOverlapping(squaresRow1, 'actor', a[2])]
+    .map(sq => sq.state.index)
+    .sort((a, b) => a - b);
+  expect(indices).toStrictEqual([0, 3]);
+});
 
 // !! NOT FINISHED !!
